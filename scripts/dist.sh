02b6774380f1aef46c79ec15c6ab4afaf9a98cec
MCAR43 <MCAR43@mst.edu>
2022-02-19 21:19:52 -0700

    Swapped to react

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -3,5 +3,6 @@
 } || {
   mkdir dist
 }
-zip dist/$npm_package_name.zip -r build package.json package-lock.json
+zip dist/$npm_package_name.zip -r build public node_modules/react-scripts/scripts package.json package-lock.json
+
 
