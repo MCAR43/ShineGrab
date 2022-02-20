1eadf6aae92277962d0f6d9d17326a90a86d1c7e
MCAR43 <MCAR43@mst.edu>
2022-02-19 21:43:43 -0700

    Fixed react deploys

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -3,6 +3,6 @@
 } || {
   mkdir dist
 }
-zip dist/$npm_package_name.zip -r build public node_modules/react-scripts/scripts package.json package-lock.json
+zip dist/$npm_package_name.zip -r build public package.json package-lock.json
 
 
